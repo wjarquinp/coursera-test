f67dc05d706652483ec76b00a04c9a18316a799c
William <wjarquin0007@gmail.com>
2021-08-03 10:11:59 -0600

    Tuesday update

---
+++ site/js/code.js
@@ -0,0 +1,23 @@
+function changePrimitive(primValue) {
+    console.log("Primitive value in change....");
+    console.log("Before");
+    console.log(primValue);
+
+    primValue = 5;
+    console.log("After");
+    console.log(primValue);
+
+}
+
+/*
+function Dog(name) {
+  this.name = name;
+}
+
+Dog.prototype.bark = function () {
+  console.log(this.name + " likes barking! Bark!");
+}
+
+var max = new Dog("Max", "Buddy");
+max.bark();
+ */
