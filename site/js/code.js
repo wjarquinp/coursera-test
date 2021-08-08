fbb9f2d99ef399cabcb571829bd5529caea2fa88
William <wjarquin0007@gmail.com>
2021-08-07 19:36:13 -0600

    module4-solution

--- site/js/code.js
+++ site/js/code.js
@@ -7,17 +7,5 @@ function changePrimitive(primValue) {
     console.log("After");
     console.log(primValue);
 
-}
-
-/*
-function Dog(name) {
-  this.name = name;
-}
-
-Dog.prototype.bark = function () {
-  console.log(this.name + " likes barking! Bark!");
-}
-
-var max = new Dog("Max", "Buddy");
-max.bark();
- */
+};
+changePrimitive(7);
