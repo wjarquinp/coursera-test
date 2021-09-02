3b7e05970177a5f89b8320e79e1c8fe7edccea04
William <wjarquin0007@gmail.com>
2021-09-01 18:42:12 -0600

    Final update

--- module5-solution/assignment5-solution-starter/js/script.js
+++ module5-solution/assignment5-solution-starter/js/script.js
@@ -99,10 +99,11 @@ function buildAndShowHomeHTML (categories) {//all the categories from the server
     function (homeHtml) {
 
       //choosing random category
-      var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
-      //I need to store the argument is paased in, in order to use it within the function
+      var chosenCategoryShortName = chooseRandomCategory(categories);
+      var category_name =  chosenCategoryShortName.short_name;
+            //I need to store the argument is paased in, in order to use it within the function
       
-      var homeHtmlToInsertIntoMainPage =  insertProperty(homeHtml,"randomCategoryShortName", chosenCategoryShortName );
+      var homeHtmlToInsertIntoMainPage =  insertProperty(homeHtml,"randomCategoryShortName", category_name );
 
       insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
       
@@ -141,7 +142,7 @@ function chooseRandomCategory (categories) {
   var randomArrayIndex = Math.floor(Math.random() * categories.length);
 
   // return category object with that randomArrayIndex
-  return categories[randomArrayIndex].short_name;
+  return categories[randomArrayIndex];
 
 }
 
